0bfadfe39278d0db03eaf619472942bc181b309e
canofranck <cano.franck77@gmail.com>
2023-03-02 13:27:26 +0100

    post recette avec ingredient et etape ok !!!!!

--- src/app/components/etape/add-etape/add-etape.component.ts
+++ src/app/components/etape/add-etape/add-etape.component.ts
@@ -1,3 +1,4 @@
+import { Etape } from './../../../models/etape/etape';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
@@ -11,8 +12,9 @@ import { RecetteService } from 'src/app/services/recette/recette.service';
 })
 export class AddEtapeComponent implements OnInit{
   declare formaddEtape: FormGroup;
-  @Input()
-  afficheretape!: boolean;
+  declare recettes : any ;
+  @Input() idrecetteencours! : number;
+  @Input()  afficheretape!: boolean;
   @Output()
    public   cacheretape:EventEmitter<any> =new EventEmitter<any>();
   constructor (
@@ -31,32 +33,63 @@ export class AddEtapeComponent implements OnInit{
           id_recette:['',Validators.required],
 
         })
+        this.recetteService.findAllRecettes().subscribe(
+          data =>{
+          // console.log(data);
+              this.recettes = Object.values(data);
+             this.recettes.sort((a: { date_recette: number; }, b: { date_recette: number; }) => (a.date_recette < b.date_recette ? 1 : -1))
+              // console.log(this.recettes);
+             this.idrecetteencours=this.recettes[0].id_recette;
+            //  console.log(" id recette avant le set"+this.idrecetteencours);
+              this.recetteService.setIdRecetteEncours( this.idrecetteencours);
+              // console.log(" id recette en cours dans affiche ingredient"+this.idrecetteencours);
+            }
+        )
   }
   create(){
+
+    const formValues = this.formaddEtape.value;
+    const etape = new Etape();
+
+      etape.numero_etape = this.formaddEtape.value.numero_etape;
+     etape.instructions_etape =  this.formaddEtape.value.instructions_etape;
+      etape.image_etape = this.formaddEtape.value.image_etape;
+      etape.id_recette=this.idrecetteencours;
+      // this.nouveau_tableau.push(ingredient);
+      // console.table(this.nouveau_tableau);
+       console.log(" id recette avant submit"+this.idrecetteencours)
+      this.etapeService.saveEtape(etape).subscribe((response) => {
+        console.log(response);
+          this.formaddEtape.reset();
+          // this.nouveau_tableau.push(response as Ingredients);
+        //
+
+       });
+
      // mehtode create avec manytoone depuis etape
-    this.recetteService.editRecette(this.formaddEtape.value.id_recette).subscribe(
-      (recette) => {
-        if(recette) {
-          this.formaddEtape.value.recette = recette;
-          this.etapeService.saveEtape(this.formaddEtape.value).subscribe(
-            () => {
-              this.router.navigate(['/etape'])
-            },
-            (error) => {
-              console.error(error);
-              // Afficher un message d'erreur à l'utilisateur
-            }
-          )
-        } else {
-          console.error("L'édition de l abonnement a échoué");
-          // Afficher un message d'erreur à l'utilisateur
-        }
-      },
-      (error) => {
-        console.error(error);
-        // Afficher un message d'erreur à l'utilisateur
-      }
-    );
+    // this.recetteService.editRecette(this.formaddEtape.value.id_recette).subscribe(
+    //   (recette) => {
+    //     if(recette) {
+    //       this.formaddEtape.value.recette = recette;
+    //       this.etapeService.saveEtape(this.formaddEtape.value).subscribe(
+    //         () => {
+    //           this.router.navigate(['/etape'])
+    //         },
+    //         (error) => {
+    //           console.error(error);
+    //           // Afficher un message d'erreur à l'utilisateur
+    //         }
+    //       )
+    //     } else {
+    //       console.error("L'édition de l abonnement a échoué");
+    //       // Afficher un message d'erreur à l'utilisateur
+    //     }
+    //   },
+    //   (error) => {
+    //     console.error(error);
+    //     // Afficher un message d'erreur à l'utilisateur
+    //   }
+    // );
   }
   cachertape(){
 
