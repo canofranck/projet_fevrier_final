cae908dae5906daba3335a7302b7a964b9554f31
canofranck <cano.franck77@gmail.com>
2023-03-15 16:43:07 +0100

    affichage par categorie de recette ok

--- src/app/recetteparcategorie/recetteparcategorie.component.ts
+++ src/app/recetteparcategorie/recetteparcategorie.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 import { Recette } from 'src/app/models/recette/recette';
 import { RecetteService } from 'src/app/services/recette/recette.service';
+import { GallerieService } from '../services/gallerie/gallerie.service';
 
 @Component({
   selector: 'app-recetteparcategorie',
@@ -12,8 +14,15 @@ export class RecetteparcategorieComponent implements OnInit {
   declare recettes : any[];
   declare categorie: string;
   declare recettesFiltrees: Recette[];
+  declare gallerie :any;
 
-  constructor(private recetteService: RecetteService, private route: ActivatedRoute) { }
+  constructor(
+    private recetteService: RecetteService,
+     private route: ActivatedRoute,
+     private gallerieService : GallerieService,
+     private router: Router,
+
+     ) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(params => {
@@ -28,5 +37,22 @@ export class RecetteparcategorieComponent implements OnInit {
         }
       );
     });
+    this.getGalleries();
+    this.router.events.pipe(filter((event: any) => event instanceof NavigationEnd))
+    .subscribe(() => {
+      console.log('Redirection effectuée !');
+      // recharger la liste des dernières recettes
+
+    });
   }
+  getGalleries() {
+    this.gallerieService.findAllGalleries().subscribe(
+       (data=>{
+         this.gallerie = data;
+  console.log(this.gallerie);
+       }
+         )
+
+     )
+   }
 }
